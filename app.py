5618ef4caa541e7de7f413c06893da220fb4268d
Ben Taljaard <btaljaard@tmns.com>
2018-03-02 12:47:08 +0100

    add health check

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ class MainHandler(tornado.web.RequestHandler):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
         self.write(os.environ['response_message'])
 
+class HealthHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("healthy")
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/health", HealthHandler),
     ])
 
 if __name__ == "__main__":
