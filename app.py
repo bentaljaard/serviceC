da595c034c0585b13b153f2a306210ad48d3490e
Ben Taljaard <btaljaard@tmns.com>
2018-03-02 11:53:26 +0100

    add initial files

---
+++ app.py
@@ -0,0 +1,18 @@
+import tornado.ioloop
+import tornado.web
+import os
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.write(os.environ['response_message'])
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
