f7a47b236bcd0e4fcdf40605f50a0a895baabecf
Ben Taljaard <ben.taljaard@devoteam.com>
2018-11-01 13:28:54 +0100

    update to show dependency output

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import tornado.ioloop
 import tornado.web
 import os
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
         # self.write(os.environ['response_message'])
-        self.write("Hello from python")
+        response_message = dict()
+        response_message["service"] = "c"
+        self.write(json.dumps(response_message))
 
 class HealthHandler(tornado.web.RequestHandler):
     def get(self):
